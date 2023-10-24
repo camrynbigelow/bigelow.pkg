c02718a27fa28a90194068f88f08edd4fb000606
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-10-24 19:30:51 +0000

    rank abundance

---
+++ bigelow.pkg/R/rank_abundance.R
@@ -0,0 +1,9 @@
+Rank abundance function
+#Plot a histogram of your data in descending order. (Will be used in thesis for species richness)
+
+rank_abundance<-function(data, x){
+  hist<-ggplot(data = , mapping=aes(x))+geom_histogram()
+  return<-hist
+}
+
+

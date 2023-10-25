08d428a0ee3d9097a9c4f895a6aa534e89619139
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-10-25 00:25:59 +0000

    edited for function package, curly braces, function doesn't work

--- bigelow.pkg/R/my_anova.R
+++ bigelow.pkg/R/my_anova.R
@@ -1,8 +1,13 @@
-my_anova
-#Create an anova summary for your data
+#'Create an anova summary for your data
+#'
+#'Compare relationships with a categorical independent variable using ANOVA
+#'@param data The dataset you read in
+#'@param x The independent variable
+#'@param y The dependent variable
+#'@return summary The ANOVA summary
 
 my_anova<-function(data, x, y){
-  model_fit<-lm(y~x, data)
+  model_fit<-lm({{y}}~{{x}}, data)
   anova_model_fit<-aov(model_fit)
   summary<-summary(anova_model_fit)
   return<- summary

a481c9447a41a016c20c1c930b962c4bba0ec954
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-10-25 15:20:01 +0000

    set up

--- bigelow.pkg/R/nmds.R
+++ bigelow.pkg/R/nmds.R
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
 #'Non-metric Multidimensional Scale
-#'
+#'#'
 #'Description
 #'@param
 #'@return
+#'
+#'@export
 
 nmds<-function(){
   
   return()
 }
-=======
-#Non-metric Multidimensional Scale
->>>>>>> 9688c0a830b5d6fbc3ba5a0acace4eb52724d83f
+
